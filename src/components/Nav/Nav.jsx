387074b416647e90327b3e0f6a490849be614151
KarolisGaiv <kgaivenis@gmail.com>
2021-08-30 11:07:38 +0300

    Add initial mobile navigation

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -18,6 +18,11 @@ function Nav() {
           <li className='navigation --link'>Cart</li>
         </Link>
       </ul>
+      <div className='navigation__mobile-nav'>
+        <div className='line1'></div>
+        <div className='line2'></div>
+        <div className='line3'></div>
+      </div>
     </nav>
   );
 }
