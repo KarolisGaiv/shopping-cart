51cef7e61386bbcea71eec288dbe283aea00b353
KarolisGaiv <kgaivenis@gmail.com>
2021-08-31 16:18:05 +0300

    Edit game card component - name height and button margin

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -18,7 +18,7 @@ function Card({ name, regularPrice, salePrice, image, handleClick }) {
       </div>
       <div className='card__details'>
         <h2 className='card__name'>{name}</h2>
-        <h3 className='card__regular-price'>Old Price $ {regularPrice}</h3>
+        <h4 className='card__regular-price'>Old Price $ {regularPrice}</h4>
         <h1 className='card__sale-price'>On Sale $ {salePrice}</h1>
       </div>
       <button onClick={addProduct} className='card__button'>
