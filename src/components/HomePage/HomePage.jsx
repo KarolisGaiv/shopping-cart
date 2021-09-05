51cefe47c33428bcf8a847fdeab10e097e1fef02
KarolisGaiv <kgaivenis@gmail.com>
2021-09-05 11:56:24 +0300

    Edit home page header positioning

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div className='content'>
-      <Link to='/shop'>
-        <button>Shop Now</button>
-      </Link>
+    <div className='home-wrapper'>
+      <div className='page-intro'>
+        <h1 className='page-intro__header'>Best Place For Games</h1>
+        <Link to='/shop'>
+          <button>Shop Now</button>
+        </Link>
+      </div>
     </div>
   );
 }
